cf7dbd1605e743e28b2acbc4e8ae72c33826991d
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-03-08 22:50:01 +0100

    simplify lint allows using inner attributes.

--- jni-bindgen/src/emit_rust/modules.rs
+++ jni-bindgen/src/emit_rust/modules.rs
@@ -17,62 +17,14 @@ impl Module {
 
         for (name, module) in self.modules.iter() {
             writeln!(out)?;
-            if indent.is_empty() {
-                writeln!(
-                    out,
-                    "#[allow(non_camel_case_types)]   // We map Java inner classes to Outer_Inner"
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(dead_code)]              // We generate structs for private Java types too, just in case."
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(deprecated)]             // We're generating deprecated types/methods"
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(non_upper_case_globals)] // We might be generating Java style fields/methods"
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(non_snake_case)]         // We might be generating Java style fields/methods"
-                )?;
-            }
+
             writeln!(out, "{}pub mod {} {{", indent, name)?;
-            writeln!(out, "{}    #[allow(unused_imports)] use super::__jni_bindgen;", indent)?;
+            writeln!(out, "{}    use super::__jni_bindgen;", indent)?;
             module.write(context, &next_indent[..], out)?;
             writeln!(out, "{}}}", indent)?;
         }
 
         for (_, structure) in self.structs.iter() {
-            if indent.is_empty() {
-                if structure.rust.struct_name.contains('_') {
-                    writeln!(
-                        out,
-                        "#[allow(non_camel_case_types)] // We map Java inner classes to Outer_Inner"
-                    )?;
-                }
-                if !structure.java.is_public() {
-                    writeln!(
-                        out,
-                        "#[allow(dead_code)] // We generate structs for private Java types too, just in case."
-                    )?;
-                }
-                writeln!(
-                    out,
-                    "#[allow(deprecated)]             // We're generating deprecated types/methods"
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(non_upper_case_globals)] // We might be generating Java style fields/methods"
-                )?;
-                writeln!(
-                    out,
-                    "#[allow(non_snake_case)]         // We might be generating Java style fields/methods"
-                )?;
-            }
-
             structure.write(context, indent, out)?;
         }
 
