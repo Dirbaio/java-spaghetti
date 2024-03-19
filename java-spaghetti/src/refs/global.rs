e9ca60044eca80fec70a5f971c72376c4dea68ff
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-03-19 23:09:56 +0100

    We assume there is only one VM, no need to check it.

--- java-spaghetti/src/refs/global.rs
+++ java-spaghetti/src/refs/global.rs
@@ -48,12 +48,7 @@ impl<T: ReferenceType> Global<T> {
     }
 
     pub fn with<'env>(&'env self, env: Env<'env>) -> Ref<'env, T> {
-        assert_eq!(self.vm, env.vm()); // Soundness check - env *must* belong to the same VM!
-        unsafe { self.with_unchecked(env) }
-    }
-
-    pub unsafe fn with_unchecked<'env>(&'env self, env: Env<'env>) -> Ref<'env, T> {
-        Ref::from_raw(env, self.object)
+        unsafe { Ref::from_raw(env, self.object) }
     }
 }
 
